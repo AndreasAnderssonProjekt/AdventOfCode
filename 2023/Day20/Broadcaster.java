ca0eef0b2e9a07d5c124bf8e5d6cf48db7017b5a
Andreas Andersson <116072490+AndreasAnderssonProjekt@users.noreply.github.com>
2023-12-20 15:06:52 +0100

    Create Broadcaster.java
    Broadcaster Class. Subclass of Module.

---
+++ 2023/Day20/Broadcaster.java
@@ -0,0 +1,26 @@
+package AdventOfCode;
+
+public class Broadcaster extends Module{
+	private Pulse curr = Pulse.LOW;
+	
+	public Broadcaster(String name) {
+		super(name);
+	}
+
+	@Override
+	public Pulse sendPulse() {
+		return this.curr;
+	}
+
+	@Override
+	public boolean readyToSend() {
+		return true;
+	}
+
+	@Override
+	public void update(Pulse incPulse, Module inputModule) {
+		this.curr = incPulse;
+		
+	}
+	
+}
