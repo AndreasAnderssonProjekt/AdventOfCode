93b46e79f85d12f173c4c94552fbeac245138a0b
Andreas Andersson <116072490+AndreasAnderssonProjekt@users.noreply.github.com>
2023-12-20 15:06:11 +0100

    Create Conjunction.java

---
+++ 2023/Day20/Conjunction.java
@@ -0,0 +1,40 @@
+package AdventOfCode;
+
+import java.util.HashMap;
+
+public class Conjunction extends Module{
+	private HashMap<Module, Pulse> memory = new HashMap<>();
+	
+	public Conjunction(String name) {
+		super(name);
+	}
+
+	@Override
+	public void update(Pulse incPulse, Module inputModule) {
+		memory.put(inputModule, incPulse);
+	}
+	
+	public void clearMemory() {
+		this.memory.clear();
+	}
+
+	@Override
+	public Pulse sendPulse() {
+		boolean allHigh = true;
+		for(Pulse p : memory.values()) {
+			if(p == Pulse.LOW ) allHigh = false;
+		}
+		if(allHigh) return Pulse.LOW;
+		else return Pulse.HIGH;
+	}
+
+	@Override
+	public boolean readyToSend() {
+		return true;
+	}
+	
+	public void addToMemory(Module m) {
+		this.memory.put(m, Pulse.LOW);
+	}
+	
+}
