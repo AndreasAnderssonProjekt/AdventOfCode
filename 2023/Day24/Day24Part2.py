f928c59193ac85dc5fdd5bfd932c5b451287290d
Andreas Andersson <116072490+AndreasAnderssonProjekt@users.noreply.github.com>
2023-12-26 00:31:29 +0100

    Create Day24Part2.py

---
+++ 2023/Day24/Day24Part2.py
@@ -0,0 +1,12 @@
+import z3
+
+hailstones = [tuple(map(int, line.replace("@", ",").split(","))) for line in open("test.txt")]
+
+rock = z3.RealVector('r', 6)
+time = z3.RealVector('t', 3)
+s = z3.Solver()
+s.add(*[rock[d] + rock[d+3] * t == hail[d] + hail[d+3] * t
+        for t, hail in zip(time, hailstones) for d in range(3)])
+s.check()
+
+print(s.model().eval(sum(rock[:3])))
